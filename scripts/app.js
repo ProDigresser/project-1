d22f848baf54f656ef6b155970aef61700431a34
Laurence Walshe <laurence_walshe@hotmail.com>
2020-10-12 17:50:57 +0100

    end of day, many bug fixes, game functions

--- scripts/app.js
+++ scripts/app.js
@@ -18,6 +18,13 @@ function startGame() {
     cellArray.push(div)
   }
 
+  // ! This assigns random backgrounds 
+  for (let i = 0; i < cellArray.length; i++){
+    const rng = Math.floor(Math.random() * 135)
+    const randomBg = `bg-${rng}`
+    cellArray[i].classList.add(randomBg)
+  }
+
   // ! This makes an array of unique random numbers
   for (let i = 0; i < (cellArray.length / 6); i++) {
     const rng = Math.floor(Math.random() * cellArray.length)
@@ -35,181 +42,219 @@ function startGame() {
 
   // ! This listens to each cell for a click
   cellArray.forEach(cell => {
+    cell.addEventListener('contextmenu', (e) => {
+      e.preventDefault()
+      if (!cell.classList.contains('safe')) {
+        cell.classList.toggle('flag')
+     
+      }
+    })
+
     cell.addEventListener('click', () => {
 
-
-      // ! DRY adjacent checkers 
-      const idLeft = Number(cell.id) - 1
-      const idTopLeft = Number(cell.id) - width - 1
-      const idTop = Number(cell.id) - width
-      const idTopRight = Number(cell.id) - width + 1
-      const idRight = Number(cell.id) + 1
-      const idBottomRight = Number(cell.id) + width + 1
-      const idBottom = Number(cell.id) + width
-      const idBottomLeft = Number(cell.id) + width - 1
-
-      // ! This grabs adjacent cells
-      const cellTop = document.getElementById(idTop)
-      const cellTopLeft = document.getElementById(idTopLeft)
-      const cellLeft = document.getElementById(idLeft)
-      const cellBottomLeft = document.getElementById(idBottomLeft)
-      const cellBottom = document.getElementById(idBottom)
-      const cellBottomRight = document.getElementById(idBottomRight)
-      const cellRight = document.getElementById(idRight)
-      const cellTopRight = document.getElementById(idTopRight)
-
-      // ! DRY boundry checkers
-      const boundTopLeft = 0
-      const boundTopRight = width - 1
-      const boundBottomRight = (width ** 2) - 1
-      const boundBottomLeft = width ** 2 - width
-
-
-      // ! This checks for valid cells to check for mines
-
       function mineCheck(cell) {
-        let bombCount = null
-        // cell = document.getElementById(cell.id)
-        cell.classList.add('safe')
+        cell.style.backgroundImage = 'none'
 
+        // ! DRY adjacent checkers 
+        const idLeft = Number(cell.id) - 1
+        const idTopLeft = Number(cell.id) - width - 1
+        const idTop = Number(cell.id) - width
+        const idTopRight = Number(cell.id) - width + 1
+        const idRight = Number(cell.id) + 1
+        const idBottomRight = Number(cell.id) + width + 1
+        const idBottom = Number(cell.id) + width
+        const idBottomLeft = Number(cell.id) + width - 1
+
+        // ! This grabs adjacent cells
+        const cellTop = document.getElementById(idTop)
+        const cellTopLeft = document.getElementById(idTopLeft)
+        const cellLeft = document.getElementById(idLeft)
+        const cellBottomLeft = document.getElementById(idBottomLeft)
+        const cellBottom = document.getElementById(idBottom)
+        const cellBottomRight = document.getElementById(idBottomRight)
+        const cellRight = document.getElementById(idRight)
+        const cellTopRight = document.getElementById(idTopRight)
+
+        // ! DRY boundry checkers
+        const boundTopLeft = 0
+        const boundTopRight = width - 1
+        const boundBottomRight = (width ** 2) - 1
+        const boundBottomLeft = width ** 2 - width
+
+        let bombCount = null
+
+        // ! This checks for valid cells to check for mines
+        cell.classList.add('safe')
+        cell.classList.remove('flag')
         if (Number(cell.id) === boundTopLeft) {
           right(); bottomRight(); bottom()
+
+          if (bombCount !== null) {
+            cell.innerHTML = bombCount
+          } else {
+            if (!cellRight.classList.contains('safe')) { mineCheck(cellRight) }
+            if (!cellBottomRight.classList.contains('safe')) { mineCheck(cellBottomRight) }
+            if (!cellBottom.classList.contains('safe')) { mineCheck(cellBottom) }
+          }
 
         } else if (Number(cell.id) === boundTopRight) {
           bottom(); bottomLeft(); left()
 
+          if (bombCount !== null) {
+            cell.innerHTML = bombCount
+          } else {
+            if (!cellBottom.classList.contains('safe')) { mineCheck(cellBottom) }
+            if (!cellBottomLeft.classList.contains('safe')) { mineCheck(cellBottomLeft) }
+            if (!cellLeft.classList.contains('safe')) { mineCheck(cellLeft) }
+          }
+
         } else if (Number(cell.id) === boundBottomLeft) {
           right(); topRight(); top()
+
+          if (bombCount !== null) {
+            cell.innerHTML = bombCount
+          } else {
+            if (!cellRight.classList.contains('safe')) { mineCheck(cellRight) }
+            if (!cellTopRight.classList.contains('safe')) { mineCheck(cellTopRight) }
+            if (!cellTop.classList.contains('safe')) { mineCheck(cellTop) }
+          }
 
         } else if (Number(cell.id) === boundBottomRight) {
           top(); topLeft(); left()
 
+          if (bombCount !== null) {
+            cell.innerHTML = bombCount
+          } else {
+            if (!cellTop.classList.contains('safe')) { mineCheck(cellTop) }
+            if (!cellTopLeft.classList.contains('safe')) { mineCheck(cellTopLeft) }
+            if (!cellLeft.classList.contains('safe')) { mineCheck(cellLeft) }
+          }
+
         } else if (Number(cell.id) < width) {
           right(); bottomRight(); bottom(); bottomLeft(); left()
+
+          if (bombCount !== null) {
+            cell.innerHTML = bombCount
+          } else {
+            if (!cellBottom.classList.contains('safe')) { mineCheck(cellBottom) }
+            if (!cellBottomRight.classList.contains('safe')) { mineCheck(cellBottomRight) }
+            if (!cellBottomLeft.classList.contains('safe')) { mineCheck(cellBottomLeft) }
+            if (!cellLeft.classList.contains('safe')) { mineCheck(cellLeft) }
+            if (!cellRight.classList.contains('safe')) { mineCheck(cellRight) }
+          }
 
         } else if (Number(cell.id) % width === width - 1) {
           top(); bottom(); bottomLeft(); left(); topLeft()
 
+          if (bombCount !== null) {
+            cell.innerHTML = bombCount
+          } else {
+            if (!cellBottom.classList.contains('safe')) { mineCheck(cellBottom) }
+            if (!cellBottomLeft.classList.contains('safe')) { mineCheck(cellBottomLeft) }
+            if (!cellTop.classList.contains('safe')) { mineCheck(cellTop) }
+            if (!cellTopLeft.classList.contains('safe')) { mineCheck(cellTopLeft) }
+            if (!cellLeft.classList.contains('safe')) { mineCheck(cellLeft) }
+          }
+
         } else if (Number(cell.id) > (width ** 2) - width - 1) {
           right(); topRight(); top(); topLeft(); left()
+
+          if (bombCount !== null) {
+            cell.innerHTML = bombCount
+          } else {
+            if (!cellTopRight.classList.contains('safe')) { mineCheck(cellTopRight) }
+            if (!cellTopLeft.classList.contains('safe')) { mineCheck(cellTopLeft) }
+            if (!cellTop.classList.contains('safe')) { mineCheck(cellTop) }
+            if (!cellRight.classList.contains('safe')) { mineCheck(cellRight) }
+            if (!cellLeft.classList.contains('safe')) { mineCheck(cellLeft) }
+          }
 
         } else if (Number(cell.id) % width === 0) {
           top(); topRight(); right(); bottomRight(); bottom()
 
-        } else {
+          if (bombCount !== null) {
+            cell.innerHTML = bombCount
+          } else {
+            if (!cellBottomRight.classList.contains('safe')) { mineCheck(cellBottomRight) }
+            if (!cellBottom.classList.contains('safe')) { mineCheck(cellBottom) }
+            if (!cellTopRight.classList.contains('safe')) { mineCheck(cellTopRight) }
+            if (!cellTop.classList.contains('safe')) { mineCheck(cellTop) }
+            if (!cellRight.classList.contains('safe')) { mineCheck(cellRight) }
+          }
 
+        } else {
           topLeft(); top(); topRight(); right(); bottomRight(); bottom(); bottomLeft(); left()
 
           if (bombCount !== null) {
-            cell.classList.add('safe')
             cell.innerHTML = bombCount
 
           } else {
-
-            if (!cellTop.classList.contains('noBombs') && !cellTop.classList.contains('safe')) {
-              // cellTop.classList.add('noBombs')
-              // cell.classList.add('noBombs')
-              mineCheck(cellTop)
-
-            }
-            if (!cellTopLeft.classList.contains('noBombs') && !cellTopLeft.classList.contains('safe')) {
-              // cellTopLeft.classList.add('noBombs')
-              // cell.classList.add('noBombs')
-              mineCheck(cellTopLeft)
-
-            }
-            if (!cellLeft.classList.contains('noBombs') && !cellLeft.classList.contains('safe')) {
-              // cellLeft.classList.add('noBombs')
-              // cell.classList.add('noBombs')
-              mineCheck(cellLeft)
-
-            }
-            if (!cellBottomLeft.classList.contains('noBombs') && !cellBottomLeft.classList.contains('safe')) {
-              // cellBottomLeft.classList.add('noBombs')
-              // cell.classList.add('noBombs')
-              mineCheck(cellBottomLeft)
-
-            }
-            if (!cellBottom.classList.contains('noBombs') && !cellBottom.classList.contains('safe')) {
-              // cellBottom.classList.add('noBombs')
-              // cell.classList.add('noBombs')
-              mineCheck(cellBottom)
-
-            }
-            if (!cellBottomRight.classList.contains('noBombs') && !cellBottomRight.classList.contains('safe')) {
-              // cellBottomRight.classList.add('noBombs')
-              // cell.classList.add('noBombs')
-              mineCheck(cellBottomRight)
-
-            }
-            if (!cellRight.classList.contains('noBombs') && !cellRight.classList.contains('safe')) {
-              // cellRight.classList.add('noBombs')
-              // cell.classList.add('noBombs')
-              mineCheck(cellRight)
-
-            }
-            if (!cellTopRight.classList.contains('noBombs') && !cellTopRight.classList.contains('safe')) {
-              // cellTopRight.classList.add('noBombs')
-              // cell.classList.add('noBombs')
-              mineCheck(cellTopRight)
-
-            }
-            cell.innerHTML = bombCount
-
+            if (!cellTop.classList.contains('safe')) { mineCheck(cellTop) }
+            if (!cellTopLeft.classList.contains('safe')) { mineCheck(cellTopLeft) }
+            if (!cellLeft.classList.contains('safe')) { mineCheck(cellLeft) }
+            if (!cellBottomLeft.classList.contains('safe')) { mineCheck(cellBottomLeft) }
+            if (!cellBottom.classList.contains('safe')) { mineCheck(cellBottom) }
+            if (!cellBottomRight.classList.contains('safe')) { mineCheck(cellBottomRight) }
+            if (!cellRight.classList.contains('safe')) { mineCheck(cellRight) }
+            if (!cellTopRight.classList.contains('safe')) { mineCheck(cellTopRight) }
           }
-
         }
-        
+
         // ! This checks valid cells and adds to the mine count
         function topLeft() {
-          if (cellArray[idTopLeft].classList.contains('bomb')) {
+          if (cellTopLeft.classList.contains('bomb')) {
             bombCount += 1
           }
         }
         function top() {
-          if (cellArray[idTop].classList.contains('bomb')) {
+          if (cellTop.classList.contains('bomb')) {
             bombCount += 1
           }
         }
         function topRight() {
-          if (cellArray[idTopRight].classList.contains('bomb')) {
+          if (cellTopRight.classList.contains('bomb')) {
             bombCount += 1
           }
         }
         function right() {
-          if (cellArray[idRight].classList.contains('bomb')) {
+          if (cellRight.classList.contains('bomb')) {
             bombCount += 1
           }
         }
         function bottomRight() {
-          if (cellArray[idBottomRight].classList.contains('bomb')) {
+          if (cellBottomRight.classList.contains('bomb')) {
             bombCount += 1
           }
         }
         function bottom() {
-          if (cellArray[idBottom].classList.contains('bomb')) {
+          if (cellBottom.classList.contains('bomb')) {
             bombCount += 1
           }
         }
         function bottomLeft() {
-          if (cellArray[idBottomLeft].classList.contains('bomb')) {
+          if (cellBottomLeft.classList.contains('bomb')) {
             bombCount += 1
           }
         }
         function left() {
-          if (cellArray[idLeft].classList.contains('bomb')) {
+          if (cellLeft.classList.contains('bomb')) {
             bombCount += 1
           }
         }
       }
 
       // ! This checks for mines
-      if (cell.classList.contains('bomb')) {
-        alert('Boom!')
+      if (cell.classList.contains('flag')) {
+        return
+      } else if (cell.classList.contains('bomb')) {
+        // alert('Boom!')
         cell.classList.add('reveal')
+        cell.removeEventListener('click', startGame)
+        
       } else {
-        console.log(cell)
+
         mineCheck(cell)
+
       }
 
     })
